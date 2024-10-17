ff36208feb78263b3515aaf3b40a1874a29d0f52
Blake Costa <blakecosta117@gmail.com>
2024-10-17 14:43:55 -0400

    Coin toss

--- src/coin_toss.cr
+++ src/coin_toss.cr
@@ -2,5 +2,21 @@
 module CoinToss
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  puts "Coin toss...\n"
+  while true
+    # Roll head
+    puts "Flipping...\n\n"
+    random_number = Random.new.rand(0..1)
+    case random_number
+    when 0 # is Rock
+      puts "Result: HEADS"
+      break
+    when 1 # is Paper
+      puts "Result: TAILS"
+      break
+    else
+      puts "idk, you shouldn't get this"
+      break
+    end
+  end
 end
